ae89cfa217625361d62c44823436c34b15a7d6f8
XRichardroidX <oguzieibehrichard@gmail.com>
2023-01-11 12:59:18 +0100

    Connected the dots

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,49 +1,60 @@
-import '../App.css';
-import React from 'react';
+import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
-class CalculatorUi extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
+class Calculator extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = ( event ) => {
+    const buttonName = event.target.name;
+    return this.setState( ( obj ) => calculate( obj, buttonName ) );
   }
 
   render() {
-    return (
+    const { total, next, operation } = this.state;
+    const result = `${total}${operation}${next}`.replace( /null/g, '' );
+    const trimedResult = result.replace( /undefined/g, '' );
 
+    return (
       <div className="calculator">
-        <div className="body">
-          <div className="screen">0</div>
-          <div className="calc-buttons">
-            <div className="buttons">
-              .
-              <div />
-              <div className=".">.</div>
-              <div className="Append" />
-              <button type="button" className="ac">AC</button>
-              <button type="button" className="both">+/-</button>
-              <button type="button" className="percentage">%</button>
-              <button type="button" className="division">÷</button>
-              <button type="button" className="seven">7</button>
-              <button type="button" className="eight">8</button>
-              <button type="button" className="nine">9</button>
-              <button type="button" className="x">x</button>
-              <button type="button" className="four">4</button>
-              <button type="button" className="five">5</button>
-              <button type="button" className="six">6</button>
-              <button type="button" className="minus">-</button>
-              <button type="button" className="one">1</button>
-              <button type="button" className="two">2</button>
-              <button type="button" className="three">3</button>
-              <button type="button" className="plus">+</button>
-              <button type="button" className="zero">0</button>
-              <button type="button" className="full-stop">.</button>
-              <button type="button" className="equals-to">=</button>
-            </div>
-          </div>
+        <input
+          type="text"
+          className="display"
+          id="calcDisplay"
+          value={trimedResult === '' ? '0' : trimedResult}
+          disabled
+        />
+
+        <div className="inputs">
+          <button type="button" name="AC" onClick={this.handleClick}>AC</button>
+          <button type="button" name="+/-" onClick={this.handleClick}>+/-</button>
+          <button type="button" name="%" onClick={this.handleClick}>%</button>
+          <button type="button" name="÷" onClick={this.handleClick}>÷</button>
+          <button type="button" name="7" onClick={this.handleClick}>7</button>
+          <button type="button" name="8" onClick={this.handleClick}>8</button>
+          <button type="button" name="9" onClick={this.handleClick}>9</button>
+          <button type="button" name="x" onClick={this.handleClick}>x</button>
+          <button type="button" name="4" onClick={this.handleClick}>4</button>
+          <button type="button" name="5" onClick={this.handleClick}>5</button>
+          <button type="button" name="6" onClick={this.handleClick}>6</button>
+          <button type="button" name="-" onClick={this.handleClick}>-</button>
+          <button type="button" name="1" onClick={this.handleClick}>1</button>
+          <button type="button" name="2" onClick={this.handleClick}>2</button>
+          <button type="button" name="3" onClick={this.handleClick}>3</button>
+          <button type="button" name="+" onClick={this.handleClick}>+</button>
+          <button type="button" name="0" onClick={this.handleClick}>0</button>
+          <button type="button" name="." onClick={this.handleClick}>.</button>
+          <button type="button" name="=" onClick={this.handleClick}>=</button>
         </div>
       </div>
     );
   }
 }
 
-export default CalculatorUi;
+export default Calculator;
